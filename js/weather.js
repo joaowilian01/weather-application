7c38494cf9afad30f62910c06cee2b4c6e2b9f01
joaowilian01 <joaowilian01@gmail.com>
2021-01-19 18:34:00 -0300

    Added a function to get a city data

--- js/weather.js
+++ js/weather.js
@@ -1,1 +1,22 @@
 const APIKey = 'X9xjlMytryPoY5Om2GlNBZA06lnEC2Yr'
+
+const getCityUrl = cityName =>
+  `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKey}&q=${cityName}`
+
+const getCityData = async cityName => {
+  try {
+    const cityUrl = getCityUrl(cityName)
+    const response = await fetch(cityUrl)
+
+    if(!response.ok) {
+      throw new Error('Não foi possível obter os dados')
+    }
+
+    const [cityData] = await response.json()
+    return cityData
+  } catch ({name, message}) {
+    alert(`${name}: ${message}`)
+  }
+}
+
+getCityData('São Paulo')
